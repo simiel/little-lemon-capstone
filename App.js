94624e0fdc935f6639e1f2d4e25670dabb8aa875
Simiel <samensah036@st.ug.edu.gh>
2024-04-29 00:11:54 +0000

    onboarding

--- App.js
+++ App.js
@@ -1,11 +1,112 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+          width: '100%',
+        }}
+      >
+        <Image
+          style={{ height: 100, width: 100 }}
+          source={require('./assets/icon.png')}
+        />
+        <Text
+          style={{
+            fontWeight: '700',
+            fontSize: 28,
+            marginTop: 20,
+            marginBottom: 20,
+          }}
+        >
+          Let us get to know you
+        </Text>
+      </View>
+      <View style={{ flex: 1, width: '100%' }}>
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: '500',
+            marginBottom: 10,
+            textAlign: 'center',
+          }}
+        >
+          First Name
+        </Text>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 2,
+            width: '100%',
+            borderRadius: 5,
+          }}
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: '500',
+            marginBottom: 10,
+            textAlign: 'center',
+          }}
+        >
+          Email
+        </Text>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 2,
+            width: '100%',
+            borderRadius: 5,
+          }}
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+
+        {/* next button */}
+        <TouchableOpacity
+          style={{
+            backgroundColor: name === '' || email === '' ? 'gray' : 'black',
+            padding: 10,
+            borderRadius: 5,
+            marginTop: 20,
+            width: '40%',
+            alignSelf: 'flex-end',
+            marginTop: 'auto',
+          }}
+          disabled={name === '' || email === ''}
+        >
+          <Text
+            style={{
+              color: 'white',
+              textAlign: 'center',
+              fontSize: 18,
+              fontWeight: '500',
+            }}
+          >
+            Next
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -16,5 +117,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
 });
