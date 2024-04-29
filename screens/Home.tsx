c5f5fe5f223badbd99cb6830a51f37f8c8eb15ca
Simiel <samensah036@st.ug.edu.gh>
2024-04-29 18:42:10 +0000

    testing the onboarding data persistence

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
+import useAuthStore from '../contexts/AuthContext';
 
 const Home = () => {
+  const { onboarded, toggleOnboarding } = useAuthStore();
   return (
     <View>
       <Text>Home</Text>
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'black',
+          padding: 10,
+          borderRadius: 5,
+          width: '40%',
+          alignSelf: 'flex-end',
+          marginTop: 'auto',
+        }}
+        onPress={() => {
+          toggleOnboarding();
+        }}
+      >
+        <Text
+          style={{
+            color: 'white',
+            textAlign: 'center',
+            fontSize: 18,
+            fontWeight: '500',
+          }}
+        >
+          Toggle
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
